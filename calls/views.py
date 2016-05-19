0a21a7b65dcdb7e6acf1c8445e9ab30ae2c490e5
Christian Rojas <christiandiegor@gmail.com>
2016-05-19 11:08:49 -0400

    manejo de excepciones

--- calls/views.py
+++ calls/views.py
@@ -5,7 +5,8 @@ import json
 import logging
 
 
-from django.http import HttpResponse
+from django.forms.models import model_to_dict
+from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
@@ -60,86 +61,95 @@ class CallInputTemplateView(TemplateView):
         logger.info(type(request.body))
         logger.info(request.body)
 
-        request_body = json.loads(request.body.decode('utf-8'))
-        logger.info("Imprimiendo json request_body")
-        logger.info(request_body)
-
-        collection = str(request_body['collection']).decode('utf-8')
-        
-        sp = str(request_body['sp']).decode('utf-8')
-        
-        key = request_body['key']
-        
-        beginCall = str(request_body['beginCall']).decode('utf-8')
-        beginCall = string_date_to_datetime(beginCall)
-
-        origin = request_body['origin']
-
-        callAnswered = str(request_body['callAnswered']).decode('utf-8')
-        if callAnswered == 'true':
-            callAnswered = True
-        else:
-            callAnswered = False
-
-        lastState = str(request_body['lastState']).decode('utf-8')
-
-        IVRSel = request_body['IVRSel']
-        
-        dialIntentBegin1 = str(request_body['dialIntentBegin1']).decode('utf-8')
-        dialIntentBegin1 = string_date_to_datetime(dialIntentBegin1)
-
-        dialIntentCaller1 = request_body['dialIntentCaller1']
-        dialIntentCalled1 = request_body['dialIntentCalled1']
-        
-        dialIntentEnd1 = str(request_body['dialIntentEnd1']).decode('utf-8')
-        dialIntentEnd1 = string_date_to_datetime(dialIntentEnd1)
-
-        dialIntentAnswered1 = str(request_body['dialIntentAnswered1']).decode('utf-8')
-        if dialIntentAnswered1 == 'true':
-            dialIntentAnswered1 = True
-        else:
-            dialIntentAnswered1 = False
-
-        sessionFile = str(request_body['sessionFile']).decode('utf-8')
-        
-        hc = str(request_body['hc']).decode('utf-8')
-        
-        routing = str(request_body['routing']).decode('utf-8')
-        
-        name = str(request_body['name']).decode('utf-8')
-        
-        endDial = str(request_body['endDial']).decode('utf-8')
-        endDial = string_date_to_datetime(endDial)
-
-        timeStamp = str(request_body['timeStamp']).decode('utf-8')
-        timeStamp = string_date_to_datetime(timeStamp)
-
-        # proceso de guardado
-        new_call = Call.objects.create(
-            collection=collection,
-            sp=sp,
-            key=key,
-            beginCall=beginCall,
-            origin=origin,
-            callAnswered=callAnswered,
-            lastState=lastState,
-            IVRSel=IVRSel,
-            dialIntentBegin1=dialIntentBegin1,
-            dialIntentCaller1=dialIntentCaller1,
-            dialIntentCalled1=dialIntentCalled1,
-            dialIntentEnd1=dialIntentEnd1,
-            dialIntentAnswered1=dialIntentAnswered1,
-            sessionFile=sessionFile,
-            hc=hc,
-            routing=routing,
-            name=name,
-            endDial=endDial,
-            timeStamp=timeStamp,
-        )
-        new_call.save()
-        return HttpResponse(new_call)
         try:
-            pass
+            request_body = json.loads(request.body.decode('utf-8'))
+            logger.info("Imprimiendo json request_body")
+            logger.info(request_body)
+        except Exception, e:
+            data = {
+                "mensaje": "error al procesar JSON \n" + e
+            }
+            return HttpResponse(json.dumps(data), status=500)
+
+        try:
+            collection = str(request_body['collection']).decode('utf-8')
+            
+            sp = str(request_body['sp']).decode('utf-8')
+            
+            key = request_body['key']
+            
+            beginCall = str(request_body['beginCall']).decode('utf-8')
+            beginCall = string_date_to_datetime(beginCall)
+
+            origin = request_body['origin']
+
+            callAnswered = str(request_body['callAnswered']).decode('utf-8')
+            if callAnswered == 'true':
+                callAnswered = True
+            else:
+                callAnswered = False
+
+            lastState = str(request_body['lastState']).decode('utf-8')
+
+            IVRSel = request_body['IVRSel']
+            
+            dialIntentBegin1 = str(request_body['dialIntentBegin1']).decode('utf-8')
+            dialIntentBegin1 = string_date_to_datetime(dialIntentBegin1)
+
+            dialIntentCaller1 = request_body['dialIntentCaller1']
+            dialIntentCalled1 = request_body['dialIntentCalled1']
+            
+            dialIntentEnd1 = str(request_body['dialIntentEnd1']).decode('utf-8')
+            dialIntentEnd1 = string_date_to_datetime(dialIntentEnd1)
+
+            dialIntentAnswered1 = str(request_body['dialIntentAnswered1']).decode('utf-8')
+            if dialIntentAnswered1 == 'true':
+                dialIntentAnswered1 = True
+            else:
+                dialIntentAnswered1 = False
+
+            sessionFile = str(request_body['sessionFile']).decode('utf-8')
+            
+            hc = str(request_body['hc']).decode('utf-8')
+            
+            routing = str(request_body['routing']).decode('utf-8')
+            
+            name = str(request_body['name']).decode('utf-8')
+            
+            endDial = str(request_body['endDial']).decode('utf-8')
+            endDial = string_date_to_datetime(endDial)
+
+            timeStamp = str(request_body['timeStamp']).decode('utf-8')
+            timeStamp = string_date_to_datetime(timeStamp)
+
+            # proceso de guardado
+            new_call = Call.objects.create(
+                collection=collection,
+                sp=sp,
+                key=key,
+                beginCall=beginCall,
+                origin=origin,
+                callAnswered=callAnswered,
+                lastState=lastState,
+                IVRSel=IVRSel,
+                dialIntentBegin1=dialIntentBegin1,
+                dialIntentCaller1=dialIntentCaller1,
+                dialIntentCalled1=dialIntentCalled1,
+                dialIntentEnd1=dialIntentEnd1,
+                dialIntentAnswered1=dialIntentAnswered1,
+                sessionFile=sessionFile,
+                hc=hc,
+                routing=routing,
+                name=name,
+                endDial=endDial,
+                timeStamp=timeStamp,
+            )
+            new_call.save()
+            data = {
+                "mensaje": "objeto creado exitosamente",
+                "objeto": model_to_dict(new_call)
+            }
+            return JsonResponse(data)
         except Exception, e:
             logger.error(e)
             return HttpResponse(e, status=500)
